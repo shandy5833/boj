3b31033086270d251ffea8da1c127902643bdceb
shandy5833 <83273783+shandy5833@users.noreply.github.com>
2023-08-27 15:04:20 +0900

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+#define ll long long
+#define pll pair<ll, ll>
+#define lcm(a, b) a *b / gcd(a, b)
+ll gcd(ll a, ll b) { return (b ? gcd(b, a % b) : a); }
+using namespace std;
+
+void solve() {
+}
+
+int main() {
+  cin.tie(0)->sync_with_stdio(0);
+
+  ll tc = 1;
+  // cin >> tc;
+  while (tc--)
+    solve();
+
+  return 0;
+}
